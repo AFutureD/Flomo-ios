4f293d18d99bfb21a441c23a1d7cb76ee7971f54
afutured <afuture0@outlook.com>
2021-01-08 12:45:57 +0800

    Initial Commit

---
+++ Shared/flomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  flomoApp.swift
+//  Shared
+//
+//  Created by AFuture on 2021/1/8.
+//
+
+import SwiftUI
+
+@main
+struct flomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
